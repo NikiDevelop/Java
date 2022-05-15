c3b338d983e267dc0251c3454a427572df0b11d7
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-05-15 12:12:13 +0200

    Update AdivinaNumero.java

--- AdivinaNumero.java
+++ AdivinaNumero.java
@@ -6,16 +6,15 @@ public class AdivinaNumero {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-
 		System.out.println("Introduce un numero del 1 al 100: ");
 
 		int ale = 0;
 		int intentos = 0;
 		int num = 0;
+		
 		//Creamos un bucle for para que nos genere un número aleatorio
 		for (int i = 0; i < 1; i++) {
 			ale = (int) (Math.random() * 100 + 1);
-
 		}
 		//El bucle while se va a estar repitiendo hasta que no sea igual el número que hemos introducido al aleatorio
 		while (num != ale) {
