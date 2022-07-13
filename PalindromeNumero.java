ced5b879fd8565d4667923371fdea5d1f6f1ad16
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-13 19:23:15 +0200

    Add files via upload

---
+++ PalindromeNumero.java
@@ -0,0 +1,27 @@
+package com.java.algoritmos;
+
+import java.util.Scanner;
+
+public class PalindromeNumero {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("¿Es palíndrome el número?:");
+		int num = teclado.nextInt();
+		
+		System.out.println(EsPalindrome(num));
+	}
+
+	public static Boolean EsPalindrome(int x) {
+		if (x < 0 || (x % 10 == 0 && x != 0)) {
+			return false;
+		}
+
+		int revertirNumero = 0;
+		while (x > revertirNumero) {
+			revertirNumero = revertirNumero * 10 + x % 10;
+			x /= 10;
+		}
+		return x == revertirNumero || x == revertirNumero / 10;
+	}
+}
