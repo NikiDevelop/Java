a3cc308997620510f4523390885ab88f00e5bde9
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-14 17:09:58 +0200

    Update Fibonacci.java

--- Algoritmos/Fibonacci.java
+++ Algoritmos/Fibonacci.java
@@ -1,13 +1,16 @@
-package com.java.algoritmos;
-
 public class Fibonacci {
 
 	public static void main(String[] args) {
-
+		//Sacamos los 10 primeros numeros de Fibonacci
 		int serie = 10, num1 = 0, num2 = 1, suma = 1;
 
 		System.out.println(num1);
-
+		/*En estas lineas lo que vamos hacer es que se vayan incrementando los valores
+		 y que vayan sumando los dos ultimos digitos...
+		por ejemplo: 1 + 2 = 3; 2 + 3 = 5; 3 + 5 = 8; 5 + 8 = 13... 
+		asi hasta que se recorra el bucle 10 veces	
+		*/
+		
 		for (int i = 0; i < serie; i++) {
 			suma = num1 + num2;
 			num1 = num2;
