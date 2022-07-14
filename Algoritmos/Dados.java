bb1a640c262d8a2318127e88a09c8d67d8f8c807
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-14 17:03:59 +0200

    Update Dados.java

--- Algoritmos/Dados.java
+++ Algoritmos/Dados.java
@@ -1,17 +1,18 @@
-package com.java.algoritmos;
-
 public class Dados {
 
 	public static void main(String[] args) {
-
+		//Creamos una aplicacion que nos genere la tirada de 3 dados 
+		//y que sume los numeros que han salido
 		int dado;
 		int suma = 0;
    		#Generamos la tirada de 3 dados 
 		System.out.println("La tirada de 3 dados es: ");
 		for (int i = 0; i < 3; i++) { 
-			dado = (int) (Math.random() * 6) + 1; #Invocamos Math random para que nos genere un numero random 
+			//Invocamos Math random para que nos genere un numero random 
+			dado = (int) (Math.random() * 6) + 1; 
 			System.out.println(dado + " "); 
-			suma += dado; #la variable suma va sumando los numeros generados 
+			//la variable suma va sumando los numeros generados 
+			suma += dado; 
 		}
 		System.out.println("Suma total: " + suma);
 	}
