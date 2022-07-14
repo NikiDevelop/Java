c6f5ec9d84627a5f099fe2b1afc5dd180a82c9f8
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-14 17:24:41 +0200

    Update ArrayEliminarRepetido.java

--- Algoritmos/ArrayEliminarRepetido.java
+++ Algoritmos/ArrayEliminarRepetido.java
@@ -1,5 +1,3 @@
-package com.java.algoritmos;
-
 import java.util.Scanner;
 
 public class ArrayEliminarRepetido {
@@ -10,22 +8,27 @@ public class ArrayEliminarRepetido {
 		char[] arr = {'1', '2', '1', '5', '0', '2', '4'};
 		
 		System.out.println("Original array: ");
-		for(int i = 0; i < arr.length;i++) { //Mostramos el array
+		//Mostramos el array original
+		for(int i = 0; i < arr.length;i++) { 
 			System.out.println("["+arr[i]+"]");
 		}
-		
-		for(int i = 0; i < arr.length; i++) { //i = 0 Primero se ejecuta el for de fuera (i) pero se ejecuta entero (j) y hasta que no termine (j) no se incrementa (i)
-			for(int j = 0; j < arr.length-1;j++) { // j = 0, j = 1, j = 2.... 
+		 //Primero se ejecuta el for de fuera (i) pero se ejecuta entero (j) y hasta que no termine (j) no se incrementa (i)
+		// j = 0, j = 1, j = 2.... 
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length-1;j++) { 
 				if(i != j) {
-					if(arr[i] == arr[j]) { //aqui hacemos la comparacion, si arr[i] es igual a arr[j]...  
-						arr[j] =' '; //... lo eliminamos o en este caso vaciamos el elemento encontrado que este repetido
+					//aqui hacemos la comparacion, si arr[i] es igual a arr[j]... 
+					//... lo eliminamos o en este caso vaciamos el elemento encontrado que este repetido
+					if(arr[i] == arr[j]) {  
+						arr[j] =' '; 
 					}
 				}
 			}
 		}
 		System.out.println("-------------------------------------");
 		System.out.println("Elementos repetidos eliminados: ");
-		for(int i = 0; i < arr.length;i++) { //Mostramos el array final
+		 //Mostramos el array final
+		for(int i = 0; i < arr.length;i++) {
 			System.out.println("["+arr[i]+"]");
 		}
 	}
