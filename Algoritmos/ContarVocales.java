075da424b8a75086ef8643055bccfa5975034b48
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-18 16:21:28 +0200

    Add files via upload

---
+++ Algoritmos/ContarVocales.java
@@ -0,0 +1,37 @@
+package com.java.ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class ContarVocales{
+
+	public static void main(String[] args) {
+		
+		//Array contar las vocales de una frase
+	
+		String texto = JOptionPane.showInputDialog(null, "Escribe una frase: ", "Insercion", JOptionPane.INFORMATION_MESSAGE);
+		texto= texto.toLowerCase();
+		
+		char vocales[]= {'a', 'e', 'i', 'o', 'u'};
+		
+		char caracterActual;
+		int contadorVocal=0;
+		boolean esVocal=false;
+
+		for(int i=0; i<texto.length(); i++) {
+			
+			caracterActual=texto.charAt(i);
+			esVocal=false;
+			for(int j=0; j<vocales.length && !esVocal; j++) {
+				if(vocales[j]==caracterActual) {
+					contadorVocal++;
+					esVocal=true;
+					
+				}
+			}
+						
+		}
+		
+		JOptionPane.showMessageDialog(null, "La frase tiene "+contadorVocal+ " vocal/es", texto, JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
