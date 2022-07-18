07ddc51250394e36aebd3656fb18243a9be1a264
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-18 16:31:30 +0200

    Add files via upload

---
+++ Algoritmos/EliminarEspacios.java
@@ -0,0 +1,32 @@
+package com.java.ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class EliminarEspacios {
+
+	public static void main(String[] args) {
+
+		// Eliminar los espacios de las frases
+
+		String texto = JOptionPane.showInputDialog(null, "Escribe una frase: ", "Insercion",
+				JOptionPane.INFORMATION_MESSAGE);
+
+		char caracterActual;
+
+		String texto_sin_espacios = "";
+
+		for (int i = 0; i < texto.length(); i++) {
+
+			caracterActual = texto.charAt(i);
+
+			if (caracterActual != ' ') {
+				texto_sin_espacios += caracterActual;
+			}
+		}
+		JOptionPane.showMessageDialog(null, texto_sin_espacios, " Resultado", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
+	
+
+
