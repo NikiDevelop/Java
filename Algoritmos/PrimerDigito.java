9c58b3c94bc11d95ceaccc336f027d0da43d2951
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-18 16:38:53 +0200

    Add files via upload

---
+++ Algoritmos/PrimerDigito.java
@@ -0,0 +1,22 @@
+package com.java.ejercicios;
+
+import java.util.Scanner;
+
+public class PrimerDigito {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		int num2 = 0;
+		int num;
+		System.out.println("Introduce un número: ");
+
+		num = teclado.nextInt();
+
+		int num3 = num;
+		while (num > 9) {
+			num2 = num /= 10;
+		}
+		System.out.println("El primer digito del numero " + num3 + " es: " + num2);
+	}
+}
